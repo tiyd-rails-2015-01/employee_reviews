d431d8aa24c690160857e62561f38da4ccef64d6
Maria <mliberov@gmail.com>
2015-01-26 14:31:55 -0500

    Add employee class exists test

--- employee_test.rb
+++ employee_test.rb
@@ -1,5 +1,10 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './employee'
 
 class EmployeeTest < Minitest::Test
+
+  def test_employee_class_exists
+    assert Employee
+  end
 end
