41e40ead2de896e6722d6e1c08af8650e3be13dc
Jesseract <jlpearce@gmail.com>
2015-01-26 14:25:49 -0500

    Create files and test for employee class

---
+++ employee_reviews.rb
@@ -0,0 +1,17 @@
+### Requirements
+
+# You want to keep track of employees (with their names and salaries), departments (with their names), and which employees are in which department.  You should be able to:
+#
+# * Create a new department (given its name).
+# * Create a new employee (given his/her name and salary).
+# * Add an employee to a department.
+# * Get an employee's name.
+# * Get an employee's salary.
+# * Get a department's name.
+# * Get a total salary for all employees in a department.
+# * Add some employee review text (a paragraph or two) to an employee.
+# * Mark whether an employee is performing satisfactorily or not satisfactorily.
+# * Give a raise to an individual.  You decide if this makes sense in dollars or in percent.
+# * Give raises to a department's employees.  You must pass this method a total dollar amount, and it must distribute the raise amounts reasonably to the department's employees.  Only employees who are performing satisfactorily should get raises.
+
+require './employee'
---
+++ reviews_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './employee_reviews'
+require './employee'
+
+class ReviewsTest < Minitest::Test
+
+  def test_employee_class_exists
+    assert Employee
+  end
+
+
+
+
+
+
+end
