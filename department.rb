7cd591835112ccbd723b42f7b4162080af26e0d5
Fallonious <FallonSkinner@gmail.com>
2015-01-26 17:17:33 -0500

    names and salaries

--- department.rb
+++ department.rb
@@ -5,10 +5,19 @@ class Department
 
   def initialize(department_name)
     @department_name = department_name
+    @employees = []
   end
 
   def department_name
     @department_name
+  end
+
+  def add_employee(employee)
+    @employees << employee
+  end
+
+  def employees
+    @employees
   end
 
 
--- pay_bonus.rb
+++ pay_bonus.rb
@@ -24,6 +24,21 @@ class PayBonusTest < Minitest::Test
     assert_equal 80000, suzy.salary
   end
 
+  def test_05_employee_has_department
+    suzy = Employee.new(name: "suzy", salary: 80000)
+    accounting = Department.new("accounting")
+    accounting.add_employee(suzy)
+    assert accounting.employees.include?(suzy)
+  end
+
+  def test_06_can_pull_employee
+    suzy = Employee.new(name: "suzy", salary: 80000)
+    timmy = Employee.new(name: "timmy", salary: 60000)
+    tommy = Employee.new(name: "tommy", salary: 75000)
+    assert_equal "suzy", suzy.name
+    assert_equal 80000, suzy.salary
+  end
+
 
     # employees_have_salaries
 
