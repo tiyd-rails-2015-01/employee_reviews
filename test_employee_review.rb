8635cc41996ce86a7cf5904906a604532f7d4fdf
Chris Foster <fosterchris22@gmail.com>
2015-01-26 14:05:20 -0500

    Test 03 employees can be added to departments.

--- test_employee_review.rb
+++ test_employee_review.rb
@@ -10,11 +10,15 @@ class EmployeeReviewTest < Minitest::Test
   end
 
   def test_02_employee_class_takes_two_arguments
-    assert Employee.new("Chad Wellington", 72000)
+    assert Employee.new("Chad Wellington", "72000")
   end
 
-
-
+  def test_03_add_employees_to_departments
+    accounting = Department.new("Accounting")
+    chad = Employee.new("Chad Wellington III")
+    accounting.add_employee("Chad Wellington III")
+    assert accounting.employees = ["Chad Wellington III"]
+  end
 
 
 
