054c90ffc70117d9348d1756e41e56ba4188fdbb
C Haden <chaden@spoonflower.com>
2015-01-26 14:03:48 -0500

    employee can be added to dept., test suite checks for same

--- department.rb
+++ department.rb
@@ -1,2 +1,10 @@
 class Department
+  def initialize( name )
+    @name = name
+    @employees = []
+  end
+
+  def addEmployee( employee )
+    @employees << employee
+  end
 end
--- employee_reviews_test.rb
+++ employee_reviews_test.rb
@@ -3,6 +3,20 @@ require 'minitest/pride'
 require './employee'
 require './department'
 
+# Create a new department (given its name). ##DONE##
+# Create a new employee (given his/her name and salary). ##DONE##
+# Add an employee to a department. ##DONE##
+# Get an employee's name.
+# Get an employee's salary.
+# Get a department's name.
+# Get a total salary for all employees in a department.
+# Add some employee review text (a paragraph or two) to an employee.
+# Mark whether an employee is performing satisfactorily or not satisfactorily.
+# Give a raise to an individual. You decide if this makes sense in dollars or in percent.
+# Give raises to a department's employees. You must pass this method a total dollar amount,
+# and it must distribute the raise amounts reasonably to the department's employees.
+# Only employees who are performing satisfactorily should get raises.
+
 class EmployeeReviewsTest < MiniTest::Test
 
   def test_department_class_exists
@@ -11,5 +25,19 @@ class EmployeeReviewsTest < MiniTest::Test
 
   def test_employee_class_exists
     assert Employee
+  end
+
+  def test_initialize_employee_with_name_and_salary
+    assert Employee.new("Oliver Twist", 1.00)
+  end
+
+  def test_initialize_department_with_name
+    assert Department.new("workhouse")
+  end
+
+  def test_can_add_employees_to_dept
+    bumble = Employee.new("Mr. Bumble", 100)
+    workhouse = Department.new("workhouse")
+    assert workhouse.addEmployee(bumble)
   end
 end
