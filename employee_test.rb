6fc57b6d6de4197b330b6e9770ed239e21c80166
Maria <mliberov@gmail.com>
2015-01-26 14:33:53 -0500

    Add department class exists test

--- employee_test.rb
+++ employee_test.rb
@@ -1,10 +1,15 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './employee'
+require './department'
 
 class EmployeeTest < Minitest::Test
 
   def test_employee_class_exists
     assert Employee
+  end
+
+  def test_department_class_exists
+    assert Department
   end
 end
