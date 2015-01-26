bfa7e2f9d3a1ad4690a67de2348dbf6164e35538
Rocky Randalls <rrandalls@gmail.com>
2015-01-26 17:27:25 -0500

    Add Department#department_raise

--- department.rb
+++ department.rb
@@ -18,4 +18,16 @@ class Department
     @employees.reduce(0.0) {|sum, employee| sum += employee.salary}
   end
 
+  def department_raise(amount)
+    @amount = amount
+    until @amount == 0
+      @employees.each do |e|
+        if e.satisfactory == true
+          e.salary = (amount * 0.05) + e.salary
+          @amount -= (amount * 0.05)
+        end
+      end
+    end
+  end
+
 end
