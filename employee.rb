480911e26c58f7f7732188b312eaccbdd238e776
Owen <owenrham@gmail.com>
2015-01-26 13:29:25 -0500

    Add asserts to tests

--- employee.rb
+++ employee.rb
@@ -1,5 +1,7 @@
 class Employee
+  attr_reader :name, :salary
   def initialize(name, salary)
-
+    @name = name
+    @salary = salary
   end
 end
--- review_tests.rb
+++ review_tests.rb
@@ -14,10 +14,15 @@ class ReviewTests < Minitest::Test
   end
 
   def test_02_employee_takes_two_arguments
-    Employee.new("Bob", 100000)
+    new_employee = Employee.new("Bob", 100000)
+    assert_equal "Bob", new_employee.name
+    assert_equal 100000, new_employee.salary
   end
 
   def test_03_department_takes_one_argument
-    Department.new("Bob")
+    new_employee = Department.new("Bob")
+    assert_equal "Bob", new_employee.name
   end
+
+  
 end
