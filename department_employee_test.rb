1e228402d8788e3ea81b7f939f9631735be8af90
Drew Smith <greenwichdrew@gmail.com>
2015-01-26 22:17:54 -0500

    up to test 10

--- department_employee_test.rb
+++ department_employee_test.rb
@@ -22,15 +22,15 @@ class EmployeeReviewTest <Minitest::Test
   end
 
   def test_04_get_employee_name
-    john = Employee.new("John", 25000)
-    assert john.name == "John"
+    steve = Employee.new("Steve", 25000)
+    assert steve.name == "Steve"
     sally = Employee.new("Sally", 25000)
     assert sally.name == "Sally"
   end
 
   def test_05_get_employee_salary
-    john = Employee.new("John", 25000)
-    assert john.salary == 25000
+    steve = Employee.new("Steve", 25000)
+    assert steve.salary == 25000
     sally = Employee.new("Sally", 25000)
     assert sally.salary == 25000
   end
@@ -48,4 +48,29 @@ class EmployeeReviewTest <Minitest::Test
     assert sports.add_employee(steve)
     assert sports.total_salary == 50000
   end
+
+  def test_08_show_review_for_employee
+    sarah = Employee.new("Sarah", 25000)
+    steve = Employee.new("Steve", 25000)
+    string = "Steve is a real let down. He surfs porn all day."
+    steve.employee_review(string)
+    assert_equal string, steve.reviews[0]
+  end
+
+  def test_09_employee_performing_satisfactorily_or_not
+    steve = Employee.new("Steve", 25000)
+    sarah = Employee.new("Sarah", 25000)
+    steve.rated_unsatisfactory
+    sarah.rated_satisfactory
+    assert_equal false, steve.rated_satisfactory?
+    assert_equal true, sarah.rated_satisfactory?
+  end
+
+  def test_10_give_raise_to_employee
+    sarah = Employee.new("Sarah", 25000)
+    sarah.give_raise(5000)
+    assert_equal 30000, sarah.salary
+  end
+
+
 end
--- employees.rb
+++ employees.rb
@@ -1,10 +1,33 @@
+require './departments'
+
 class Employee
 
-  attr_reader :name, :salary
+  attr_reader :name, :salary, :reviews, :satisfactory
 
   def initialize(name, salary)
     @name = name
     @salary = salary
+    @reviews = []
+  end
+
+  def employee_review(string)
+    @reviews << string
+  end
+
+  def rated_unsatisfactory
+    @satisfactory = false
+  end
+
+  def rated_satisfactory
+    @satisfactory = true
+  end
+
+  def rated_satisfactory?
+    return @satisfactory
+  end
+
+  def give_raise(raise)
+    @salary = raise + @salary
   end
 
 end
