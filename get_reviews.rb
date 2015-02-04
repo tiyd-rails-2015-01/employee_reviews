51c3448c352c303d9ebacf9984ef09f9d4556306
Ashley Ellis <anellis12@gmail.com>
2015-02-03 19:21:54 -0500

    Add Get Reviews file to github

---
+++ get_reviews.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './employee'
+require './department'
+
+employee1= Employee.new("Xavier Smith", 100000)
+employee1.review= "Xavier is a huge asset to SciMed and is a pleasure to work with.  He quickly knocks out tasks assigned to him, implements code that rarely needs to be revisited, and is always willing to help others despite his heavy workload.  When Xavier leaves on vacation, everyone wishes he didn't have to go
+Last year, the only concerns with Xavier performance were around ownership.  In the past twelve months, he has successfully taken full ownership of both Acme and Bricks, Inc.  Aside from some false starts with estimates on Acme, clients are happy with his work and responsiveness, which is everything that his managers could ask for.
+"
